1654e9b332be61b44f851f04405905a8b9fb1d8b
Rabah Babaci <4rabah@gmail.com>
2020-05-26 12:08:42 -0700

    Delete northwind Session.sql

--- northwind Session.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-  O.Id as "Order Id",
-  CompanyName as "Customer's Company Name",
-  LastName as "Employee's Last Name"
-FROM [Order] as O
-JOIN Customer as C ON O.CustomerId = C.Id
-JOIN Employee as E ON O.EmployeeId = E.Id;
