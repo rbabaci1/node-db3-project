e9d3520bc8cf099f65dca926c469d8bdab269785
rabah babaci <4rabah@gmail.com>
2020-05-26 12:18:55 -0700

    create the findById helper that gets all the schemes

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,5 +1,6 @@
 const db = require("../data/dbConfig");
 
 const find = () => db("schemes");
+const findById = id => db("schemes").where({ id }).first();
 
-module.exports = { find };
+module.exports = { find, findById };
