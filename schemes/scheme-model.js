8ec9d123c63f5ee6004fe0a39fc33c45147351c6
rabah babaci <4rabah@gmail.com>
2020-05-26 13:53:26 -0700

    create the findSteps helper that gets all the schemes

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,5 +2,12 @@ const db = require("../data/dbConfig");
 
 const find = () => db("schemes");
 const findById = id => db("schemes").where({ id }).first();
+const findSteps = scheme_id => {
+  return db("steps as st")
+    .select("st.id", "sc.scheme_name", "st.step_number", "st.instructions")
+    .join("schemes as sc", "st.scheme_id", "sc.id")
+    .where({ scheme_id })
+    .orderBy("step_number");
+};
 
-module.exports = { find, findById };
+module.exports = { find, findById, findSteps };
